4637b4bd861fa5ae81cbed01df4097fc3895bf3f
Md. Mehedy Hasan Tanvir <mehedytanvir451@gmail.com>
2026-05-30 13:34:47 +0600

    swagger added to the logindto

--- src/modules/auth/dto/login-dto.ts
+++ src/modules/auth/dto/login-dto.ts
@@ -1,10 +1,19 @@
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from 'node_modules/@nestjs/swagger/dist/decorators/api-property.decorator';
 
 export class LoginDto {
+  @ApiProperty({
+    description: 'The email address of the user',
+    example: 'user@example.com',
+  })
   @IsEmail({}, { message: 'Please provide a valid email address' })
   @IsNotEmpty({ message: 'Email is required' })
   email!: string;
 
+  @ApiProperty({
+    description: 'The password of the user',
+    example: 'password123',
+  })
   @IsString({ message: 'Password must be a string' })
   @IsNotEmpty({ message: 'Password is required' })
   password!: string;

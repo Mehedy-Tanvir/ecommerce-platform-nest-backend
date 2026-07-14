52a1a41b4509d587f900ff766c2d251d3067c4e1
Md. Mehedy Hasan Tanvir <mehedytanvir451@gmail.com>
2026-07-14 12:40:33 +0600

    feat: configure Redis CacheModule with cache-manager-redis-yet

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './modules/auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersModule } from './modules/users/users.module';
 import { CategoryModule } from './modules/category/category.module';
 import { ProductsModule } from './modules/products/products.module';
@@ -11,16 +11,33 @@ import { OrdersModule } from './modules/orders/orders.module';
 import { ThrottlerModule } from '@nestjs/throttler';
 import { PaymentsModule } from './modules/payments/payments.module';
 import { TrpcModule } from './trpc/trpc.module';
+import { CacheModule } from '@nestjs/cache-manager';
+import { redisStore } from 'cache-manager-redis-yet';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
     ThrottlerModule.forRoot([
       {
-        ttl: 60, //seconds
-        limit: 10, //10 requests per minute
+        ttl: 60,
+        limit: 10,
       },
     ]),
+    CacheModule.registerAsync({
+      isGlobal: true,
+      useFactory: async (configService: ConfigService) => ({
+        stores: [
+          await redisStore({
+            socket: {
+              host: configService.get('REDIS_HOST', 'localhost'),
+              port: +configService.get('REDIS_PORT', 6379),
+            },
+          }),
+        ],
+        ttl: +configService.get('REDIS_TTL', 60),
+      }),
+      inject: [ConfigService],
+    }),
     PrismaModule,
     TrpcModule,
     AuthModule,
